fe630575c4082d462c5f43962b54e67f649d95db
skwich <a.3066@mail.ru>
2026-06-07 23:27:10 +0500

    update sign-up page

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,6 +1,53 @@
 'use client';
 
+import { SyntheticEvent, useState } from "react";
+import { authClient } from "@/lib/auth-client";
+
+async function OnClickRegisterButton({email, password, username}: {email:string, password: string, username: string}) {
+  const { data, error } = await authClient.signUp.email({
+          email: email,
+          name: "user",
+          password: password,
+          username: username
+      }, {
+          onRequest: (ctx) => {
+              //show loading
+          },
+          onSuccess: (ctx) => {
+              //redirect to the dashboard or sign in page
+          },
+          onError: (ctx) => {
+              // display the error message
+              alert(ctx.error.message);
+          },
+  });
+}
+
 export default function RegistrationPage() {
+  const [login, setLogin] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(event: SyntheticEvent<HTMLFormElement, SubmitEvent>) {
+    event.preventDefault();
+    const { data, error } = await authClient.signUp.email({
+        email: email,
+        name: "user",
+        password: password,
+        username: login 
+    }, {
+        onRequest: (ctx) => {
+            // show loading
+        },
+        onSuccess: (ctx) => {
+            // redirect to the dashboard or sign in page
+        },
+        onError: (ctx) => {
+            alert(ctx.error.message);
+        },
+    });
+  }
+
   return (
     <div className="min-h-screen bg-white text-black flex flex-col justify-center items-center p-6 font-sans">
 
@@ -14,9 +61,11 @@ export default function RegistrationPage() {
           </p>
         </div>
 
-        <form className="mt-[17px] flex flex-col items-center w-full">
+        <form onSubmit={handleSubmit} className="mt-[17px] flex flex-col items-center w-full">
           <div className="relative w-full">
               <input
+                value={login}
+                onChange={(e) => setLogin(e.target.value)}
                 type="text"
                 placeholder="Логин"
                 className="w-full text-black font-inter placeholder-[#9F9F9F] placeholder:text-[18px] border-b-3 border-b-[#D0D0D0] py-4 outline-none"
@@ -25,6 +74,8 @@ export default function RegistrationPage() {
 
           <div className="relative w-full">
             <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               type="email"
               placeholder="E-mail"
               className="w-full text-black font-inter placeholder-[#9F9F9F] placeholder:text-[18px] border-b-3 border-b-[#D0D0D0] py-4 outline-none"
@@ -33,6 +84,8 @@ export default function RegistrationPage() {
 
           <div className="relative w-full">
             <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               type="password"
               placeholder="Пароль"
               className="w-full text-black font-inter placeholder-[#9F9F9F] placeholder:text-[18px] border-b-3 border-b-[#D0D0D0] py-4 outline-none"
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function Page() {
-  redirect("myprojects");
+  redirect("auth/sign-up");
 }
