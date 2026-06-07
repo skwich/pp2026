4642247587f64a4eea9d07af00f2454f81faa7b4
skwich <a.3066@mail.ru>
2026-06-07 23:43:36 +0500

    update sign-in page

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,46 +1,65 @@
 'use client';
 
+import { authClient } from "@/lib/auth-client";
+import { SyntheticEvent, useState } from "react";
+
 export default function LoginPage() {
-    return (
-    <div className="min-h-screen bg-white text-black flex flex-col justify-center items-center p-6 font-sans">
 
-      <div className="w-full max-w-[430px]">
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
 
-        <div>
-          <h1 className="text-[32px] font-normal">ВХОД</h1>
-          <p className="text-[18px] font-light">
-            Нет аккаунта?{' '}
-            <a href="/register" className="hover:underline">Зарегистрироваться</a>
-          </p>
-        </div>
+  async function handleSubmit(event: SyntheticEvent<HTMLFormElement, SubmitEvent>) {
+    event.preventDefault();
+    const { data, error } = await authClient.signIn.username({
+      username: login,
+      password: password,
+    });
+  }
 
-        <form className="mt-[17px] flex flex-col items-center w-full">
-          <div className="relative w-full">
-              <input
-                type="text"
-                placeholder="Логин"
-                className="w-full text-black font-inter placeholder-[#9F9F9F] placeholder:text-[18px] border-b-3 border-b-[#D0D0D0] py-4 outline-none"
-              />
-            </div>
+  return (
+  <div className="min-h-screen bg-white text-black flex flex-col justify-center items-center p-6 font-sans">
 
-          <div className="relative w-full">
+    <div className="w-full max-w-[430px]">
+
+      <div>
+        <h1 className="text-[32px] font-normal">ВХОД</h1>
+        <p className="text-[18px] font-light">
+          Нет аккаунта?{' '}
+          <a href="/auth/sign-up" className="hover:underline">Зарегистрироваться</a>
+        </p>
+      </div>
+
+      <form onSubmit={handleSubmit} className="mt-[17px] flex flex-col items-center w-full">
+        <div className="relative w-full">
             <input
-              type="password"
-              placeholder="Пароль"
+              value={login}
+              onChange={e => setLogin(e.target.value)}
+              type="text"
+              placeholder="Логин"
               className="w-full text-black font-inter placeholder-[#9F9F9F] placeholder:text-[18px] border-b-3 border-b-[#D0D0D0] py-4 outline-none"
             />
           </div>
 
-          <button
-            type="submit"
-            className="flex items-center justify-center w-full max-w-[300px] h-full max-h-[50px] bg-[#252525] font-inter text-white text-[18px] font-normal rounded-[4px] py-[14px] mt-[50px]"
-          >
-            Войти
-          </button>
-        </form>
+        <div className="relative w-full">
+          <input
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+            placeholder="Пароль"
+            className="w-full text-black font-inter placeholder-[#9F9F9F] placeholder:text-[18px] border-b-3 border-b-[#D0D0D0] py-4 outline-none"
+          />
+        </div>
 
-      </div>
-      
+        <button
+          type="submit"
+          className="flex items-center justify-center w-full max-w-[300px] h-full max-h-[50px] bg-[#252525] font-inter text-white text-[18px] font-normal rounded-[4px] py-[14px] mt-[50px]"
+        >
+          Войти
+        </button>
+      </form>
+
     </div>
-    );
+    
+  </div>
+  );
 }
